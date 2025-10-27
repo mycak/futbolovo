a45d61e451c8c883e0429ec925902dc61e5c37a1
Piotr Myszkiewicz <mycak@wp.pl>
2025-10-27 15:54:50 +0100

    fix(layout): change ts-expect-error to ts-ignore for compatibility

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Protest_Guerrilla } from 'next/font/google';
 import clsx from 'clsx';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-expect-error
+//@ts-ignore
 import '../../globals.css';
 import Script from 'next/script';
 import { mainPagesMetadata } from '@/constants/metadata';
